5db7de06b0e58878ba7f22a6a7b758d019d1e0e2
0xMaverics <faisaa-finance@proton.me>
2023-04-24 01:03:05 +0500

    Initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
-
+import Script from 'next/script'
+import {Helmet} from "react-helmet";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  
+  <Component {...pageProps} />
+  <Helmet>
+   <Script src="js/jquery.bundle.js" type="text/javascript" ></Script>
+   <Script src="js/scripts.js" type="text/javascript"></Script>
+   <Script src="js/charts.js" type="text/javascript" ></Script>
+   <Script src="js/toastr.examples.js" type="text/javascript" ></Script>
+</Helmet>
+  </>
+  )
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,31 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
+      <Head>
+        <link rel="stylesheet" href="css/vendor.bundle.css?ver=195" />
+        <link rel="stylesheet" href="css/theme.css?ver=195" />
+        <link rel="stylesheet" href="css/style-zinnia.css?ver=195" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@900&display=swap"
+          rel="stylesheet"
+        
+          /> 
+         
+          </Head>
+      <body className="nk-body body-wider mode-onepage">
+     
+        <Main/>
+       
+        <NextScript/>
+         
       </body>
+      
+    
     </Html>
   )
 }
