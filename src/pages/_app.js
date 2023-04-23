0b3c88d907b4539e05533eb5ec919bb5d60e9e1a
0x-m1cro <0xm1cro@proton.me>
2023-04-23 20:59:26 +0500

    fixed js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,18 @@
 import Script from 'next/script'
-import {Helmet} from "react-helmet";
 export default function App({ Component, pageProps }) {
   return (
   <>
   
   <Component {...pageProps} />
   
-   <Script src="js/jquery.bundle.js" type="text/javascript" ></Script>
-   <Script src="js/scripts.js" type="text/javascript"></Script>
-   <Script src="js/charts.js" type="text/javascript" ></Script>
-   <Script src="js/toastr.examples.js" type="text/javascript" ></Script>
+   <Script src="js/jquery.bundle.js" 
+           strategy="beforeInteractive"/>
+   <Script src="js/scripts.js" 
+           strategy="beforeInteractive"/>
+   <Script src="js/charts.js" 
+           strategy="beforeInteractive"/>
+   <Script src="js/toastr.examples.js" 
+           strategy="beforeInteractive"/>
 
   </>
   )
